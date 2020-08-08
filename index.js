be58802ea3e920a06251fb0400e2bc82ad921faa
Ashok Kumaran <iamashokachu@outlook.com>
2020-08-09 03:49:24 +0530

    Phase 1 - (WIthout Collapse Concept)

---
+++ index.js
@@ -0,0 +1,58 @@
+var data = document.getElementById('displaySection');
+async function getSection(a){
+    try{
+    let sectionArray = ["World","us","politics","nyregion","Business","Opinion","technology","Science","Health","Sports","Arts","Books","fashion","Food","Travel","Magazine","T-Magazine","Real Estate", "movies","automobiles","insider", "obituaries", "sundayreview", "theater",  "upshot"];
+    let months = ["Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
+    let sectionSelect = sectionArray[a-1];
+    let myAPI = 'rbsfRyXqXYtw4IFEGjyIYygrgl1uGJ8y';
+    let url = `https://api.nytimes.com/svc/topstories/v2/${sectionSelect}.json?api-key=${myAPI}`;
+    var value = await fetch(url);
+    var input = await value.json();
+    let row = document.createElement('div');
+    row.className = 'row mt-3';
+    for(var i=0; i<input.results.length; i++)
+    {
+        var col = document.createElement('div');
+        col.className = 'col col-12 mt-3 mr-3';
+        let myDate = new Date(input.results[i].created_date);
+        let createdMonth = months[myDate.getMonth()];
+        let createdDate = myDate.getDate();
+        let createdPeriod = createdMonth + " " + createdDate;
+        if(input.results[i].section=="us")
+        {
+            let convert = input.results[i].section;
+            input.results[i].section = convert.toUpperCase();
+        }
+        else if(input.results[i].section=="nyregion")
+        {
+            input.results[i].section = "New York";
+        }
+        printArea = `<div class = 'card container'>
+        <div class = 'row xs-column-reverse'>
+        <div class = 'col-md-8 top-padding mb-3'>
+        <p class = 'card-text'>
+        <strong><span style="color:blue; text-transform: capitalize;">${input.results[i].section}</span></strong><br><br>
+        <span class="title">${input.results[i].title}</span>
+        <div class="d-flex justify-content-between">
+        <span style="color : gray"><b>${createdPeriod}</b></span><br><br>
+        <span class="byLine">${input.results[i].byline}</span>
+        </div><br>
+        <span>${input.results[i].abstract}</span><br><br>
+        <a href="${input.results[i].url}">Continue Reading</a>
+        </div>
+        <div class="col-md-4 right-padding">
+        <img src = '${input.results[i].multimedia[0].url}' class = 'card-img' id = 'image'>
+        </p>
+        </div>
+        </div>
+        </div>
+        `;
+        col.innerHTML = printArea;
+        row.appendChild(col);
+        data.appendChild(row);
+    }
+    }
+    catch{
+        alert('Try loading the page once again');
+    }
+}
